13c9deb839ca47a53dde8390874d7fe13717a1b1
David <16david93@gmail.com>
2026-02-28 21:52:32 -0800

    add validation constraints to user schemas

--- app/schemas.py
+++ app/schemas.py
@@ -7,8 +7,8 @@ class ShortUrlResponse(BaseModel):
     short_url: str
 
 class UserCreate(BaseModel):
-    username: str
-    password: str = Field(min_length=8)
+    username: str = Field(min_length=2, max_length=30)
+    password: str = Field(min_length=8, max_length=100)
 
 class UserResponse(BaseModel):
     id: int
